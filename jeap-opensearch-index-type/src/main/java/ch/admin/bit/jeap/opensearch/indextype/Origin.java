7f5801963fa448fa33f415b0392443d15ed721fb
Fabien Cerf <fabien.cerf@bit.admin.ch>
2026-06-03 10:23:00 +0200

    JEAP-6862: refactor the attribute reference in Origin as Map<String, String> instead of JsonNode

--- jeap-opensearch-index-type/src/main/java/ch/admin/bit/jeap/opensearch/indextype/Origin.java
+++ jeap-opensearch-index-type/src/main/java/ch/admin/bit/jeap/opensearch/indextype/Origin.java
@@ -1,19 +1,21 @@
 package ch.admin.bit.jeap.opensearch.indextype;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.JsonNode;
 
 import java.time.Instant;
+import java.util.Map;
 
 /**
  * Origin is the business object
- * @param id id of the business object
- * @param version version of the business object
- * @param bpId business partner id
- * @param tenant tenant of the business object
- * @param created creation timestamp of the business object
- * @param modified modified timestamp of the business object
- * @param reference deep link to the business object
+ *
+ * @param id        id of the business object
+ * @param version   version of the business object
+ * @param bpId      business partner id
+ * @param tenant    tenant of the business object
+ * @param created   creation timestamp of the business object
+ * @param modified  modified timestamp of the business object
+ * @param reference reference to the business object (e.g. url or other identifier)
+ *                  that can used to retrieve the original business object
  */
 public record Origin(String id,
                      String version,
@@ -21,6 +23,6 @@ public record Origin(String id,
                      String tenant,
                      Instant created,
                      Instant modified,
-                     JsonNode reference
+                     Map<String, String> reference
 ) {
 }
